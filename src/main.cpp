eeef5118584fc61b8f69734cf37ba977a8c44a80
tkleinloog <tkleinloog@gmail.com>
2023-12-12 21:59:34 +0100

    Progress so far getting raylib linked with cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
-//#include "raylib.h"
 #include <iostream>
+#include "../raylib/raylib.h"
 
 int main()
 {
+    const int window_width = 640;
+    const int window_height = 480;
+
+    InitWindow(window_width, window_height, "Hello World");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Hello World", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
     std::cout << "Hello World! you noob" << std::endl;
     return 0;
 }
